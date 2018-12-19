d59126c5797b1ea9a04b30c18b6c00a7c70be022
dell <641673461@qq.com>
2018-12-19 18:17:19 +0800

    添加

--- dubbo-samples-zookeeper/src/main/java/org/apache/dubbo/samples/impl/AnnotatedGreetingService.java
+++ dubbo-samples-zookeeper/src/main/java/org/apache/dubbo/samples/impl/AnnotatedGreetingService.java
@@ -27,6 +27,7 @@ public class AnnotatedGreetingService implements GreetingService {
 
     public String sayHello(String name) {
         System.out.println("greeting service received: " + name);
+		System.out.println("greeting service received: " + name);
         return "hello, " + name;
     }
 
